d2ae35ba1e51e237a84d2f3f1cc0f14a62249ef1
SIqi-Yu <1572623341@qq.com>
2022-10-29 19:29:25 +0800

    Create credit_app.py

---
+++ credit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+
+st.title('')
+df = pd.read_csv('train.csv')
+
+income_filter = st.slider('choose income:', 7005.93, 179987.28, 10000)  # min, max, default 滑块
+
+
+# filter by income
+df = df[df.Annual_Income >= income_filter]
+
+# filter by the occupation
+
+# filter by payment behavior
+
+st.subheader('Histogram of the Median House Price')
+df.Credit_Score.hist(bins=30)
